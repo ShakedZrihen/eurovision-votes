43ad90556c4b020bc4fd6547d94a74048a866973
ronyzrihen <ronyzn3@gmail.com>
2024-05-11 20:00:28 +0300

    song by contry list added

--- src/pages/Bets/songList.js
+++ src/pages/Bets/songList.js
@@ -1,5 +1,29 @@
 const SONG_BY_COUNTRY = [
-  { name: "Albania", flag: "", artist: "Anxhela Peristeri", song: "Karma", notes: "", givenPoints: 0 }
+  { "name": "Sweden", "flag": "🇸🇪", "artist": "Marcus & Martinus", "song": "Unforgettable", "notes": "", "givenPoints": 0 },
+  { "name": "Ukraine", "flag": "🇺🇦", "artist": "alyona alyona & Jerry Heil", "song": "Teresa & Maria", "notes": "", "givenPoints": 0 },
+  { "name": "Germany", "flag": "🇩🇪", "artist": "ISAAK", "song": "Always On The Run", "notes": "", "givenPoints": 0 },
+  { "name": "Luxembourg", "flag": "🇱🇺", "artist": "TALI", "song": "Fighter", "notes": "", "givenPoints": 0 },
+  { "name": "Israel", "flag": "🇮🇱", "artist": "Eden Golan", "song": "Hurricane", "notes": "", "givenPoints": 0 },
+  { "name": "Lithuania", "flag": "🇱🇹", "artist": "Silvester Belt", "song": "Luktelk", "notes": "", "givenPoints": 0 },
+  { "name": "Spain", "flag": "🇪🇸", "artist": "Nebulo", "song": "ZORRA", "notes": "", "givenPoints": 0 },
+  { "name": "Estonia", "flag": "🇪🇪", "artist": "5MIINUST x Puuluup", "song": "(nendest) narkootikumidest ei tea me (küll) midagi", "notes": "", "givenPoints": 0 },
+  { "name": "Ireland", "flag": "🇮🇪", "artist": "Bambie Thug", "song": "Doomsday Blue", "notes": "", "givenPoints": 0 },
+  { "name": "Latvia", "flag": "🇱🇻", "artist": "Dons", "song": "Hollow", "notes": "", "givenPoints": 0 },
+  { "name": "Greece", "flag": "🇬🇷", "artist": "Marina Satti", "song": "ZARI", "notes": "", "givenPoints": 0 },
+  { "name": "United Kingdom", "flag": "🇬🇧", "artist": "Olly Alexander", "song": "Dizzy", "notes": "", "givenPoints": 0 },
+  { "name": "Norway", "flag": "🇳🇴", "artist": "Gåte", "song": "Ulveham", "notes": "", "givenPoints": 0 },
+  { "name": "Italy", "flag": "🇮🇹", "artist": "Angelina Mango", "song": "La Noia", "notes": "", "givenPoints": 0 },
+  { "name": "Serbia", "flag": "🇷🇸", "artist": "TEYA DORA", "song": "RAMONDA", "notes": "", "givenPoints": 0 },
+  { "name": "Finland", "flag": "🇫🇮", "artist": "Windows95man", "song": "No Rules!", "notes": "", "givenPoints": 0 },
+  { "name": "Portugal", "flag": "🇵🇹", "artist": "", "song": "Grito", "notes": "", "givenPoints": 0 },
+  { "name": "Armenia", "flag": "🇦🇲", "artist": "LADANIVA", "song": "Jako", "notes": "", "givenPoints": 0 },
+  { "name": "Cyprus", "flag": "🇨🇾", "artist": "Silia Kapsis", "song": "Liar", "notes": "", "givenPoints": 0 },
+  { "name": "Switzerland", "flag": "🇨🇭", "artist": "", "song": "The Code", "notes": "", "givenPoints": 0 },
+  { "name": "Slovenia", "flag": "🇸🇮", "artist": "", "song": "Veronika", "notes": "", "givenPoints": 0 },
+  { "name": "Croatia", "flag": "🇭🇷", "artist": "Baby Lasagna", "song": "Rim Tim Tagi Dim", "notes": "", "givenPoints": 0 },
+  { "name": "Georgia", "flag": "🇬🇪", "artist": "", "song": "Firefighter", "notes": "", "givenPoints": 0 },
+  { "name": "France", "flag": "🇫🇷", "artist": "Slimane", "song": "Mon Amour", "notes": "", "givenPoints": 0 },
+  { "name": "Austria", "flag": "🇦🇹", "artist": "Kaleen", "song": "We Will Rave", "notes": "", "givenPoints": 0 }
 ];
 
 export default SONG_BY_COUNTRY;
