4eb58cfc07b02bcfe1ab219d91442b2df91c1a12
ShakedZrihen <shaked.zrihen@gmail.com>
2024-05-11 20:03:22 +0300

    feat: create country component

--- src/pages/Bets/index.jsx
+++ src/pages/Bets/index.jsx
@@ -1,15 +1,24 @@
 import { styled } from "@mui/material";
 import { useState } from "react";
+import CountryDescription from "./components/CountryDescription";
 
 const StyledContentContainer = styled("div")({
   height: "55vh",
   backgroundColor: "#fff"
 });
 
-
 const BetsPage = () => {
-  const [countryIndex, setCountry] = useState(0);
-  return <StyledContentContainer>Bets</StyledContentContainer>;
+  // const [countryIndex, setCountry] = useState(0);
+
+  const updateUserInfo = (notes, rating) => {
+
+  }
+  
+  return (
+    <StyledContentContainer>
+      <CountryDescription name={'Israel'} song={'Hurricane'} index={6} artist={'Eden Golan'} flag={'🇮🇱'} />
+    </StyledContentContainer>
+  );
 };
 
 export default BetsPage;
